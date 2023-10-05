58fc4d0349b57e4a37a3606387f82fb2783448c7
Kevin <kevinsilvarj10@gmail.com>
2023-10-05 14:57:25 -0300

    Página finalizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.html"],
+  content: [
+    "./public/*.html",
+    "./src/**/*.js"
+],
   theme: {
     extend: {
       colors: {
